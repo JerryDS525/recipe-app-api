a70c5c9b26a80de7e93fd9f406eaaf92ec64cc34
JerryDS525 <jerry.ds525@gmail.com>
2023-05-16 14:25:04 +0530

    postgresql v4

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,9 +6,9 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
+
+
     """Test the Calc module."""
-
-
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
