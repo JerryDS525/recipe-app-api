1fefbb159b5346ffe72ac5ccd618039b25ab6f4e
JerryDS525 <jerry.ds525@gmail.com>
2023-05-17 15:55:52 +0530

    db configured

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,14 +1,14 @@
 """
 Sample Tests.
 """
-
 from django.test import SimpleTestCase
+
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-
-
     """Test the Calc module."""
+
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
